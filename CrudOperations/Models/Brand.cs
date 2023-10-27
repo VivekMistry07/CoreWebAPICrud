c1882fa81f01c2635fc9d1b7d567a30e21a00c80
DESKTOP-PGHPJCH\AppUnik <vivek.mistry@appunik.com>
2023-10-27 10:55:08 +0530

    Add project files.

---
+++ CrudOperations/Models/Brand.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudOperations.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? category { get; set; }
+        public int IsActive { get; set; }
+    }
+}
